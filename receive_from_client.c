84c5dc1a58d04f7aab65a8bf21ff369a909c1b85
grizeus <zaytsewww@gmail.com>
2024-03-19 16:17:19 +0200

    add recv_len variable to receive_from_client() func to correct calculate and passing the length of the received message

--- receive_from_client.c
+++ receive_from_client.c
@@ -7,14 +7,14 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 
-char* receive_from_client(int fd, struct sockaddr_in* client_addr) {
+char* receive_from_client(int fd, struct sockaddr_in* client_addr, ssize_t* recv_len) {
 
     char buffer[MAX_BUFF_SIZE];
 
     socklen_t client_addr_len = sizeof(*client_addr);
-    ssize_t recv_len = recvfrom(fd, buffer, MAX_BUFF_SIZE, 0, (struct sockaddr*)client_addr, &client_addr_len);
+    *recv_len = recvfrom(fd, buffer, MAX_BUFF_SIZE, 0, (struct sockaddr*)client_addr, &client_addr_len);
 
-    if (recv_len == -1) {
+    if (*recv_len == -1) {
         if (errno != EAGAIN && errno != EWOULDBLOCK) {
             perror("Receive failed");
         }
@@ -23,9 +23,12 @@ char* receive_from_client(int fd, struct sockaddr_in* client_addr) {
 
     printf("Received from %s:%d\n", inet_ntoa(client_addr->sin_addr), htons(client_addr->sin_port));
 
-    char* msg = malloc(recv_len + 1);
-    memcpy(msg, buffer, recv_len);
-    msg[recv_len] = '\0';
+    // char* msg = malloc(*recv_len + 1);
+    // memcpy(msg, buffer, *recv_len);
+    // msg[*recv_len] = '\0';
+
+    char* msg = malloc(*recv_len);
+    memcpy(msg, buffer, *recv_len);
 
     return msg;
 }
--- receive_from_client.h
+++ receive_from_client.h
@@ -2,8 +2,9 @@
 #define RECEIVE_FROM_CLIENT_H
 
 #include <netinet/in.h>
+#include <sys/types.h>
 
 #define MAX_BUFF_SIZE  2048
 
-char* receive_from_client(int fd, struct sockaddr_in* client_addr);
+char* receive_from_client(int fd, struct sockaddr_in* client_addr, ssize_t* recv_len);
 #endif // !RECEIVE_FROM_CLIENT_H
