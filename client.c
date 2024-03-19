bb3268ccb7533507368d89fd9ed079bf466e7b97
grizeus <zaytsewww@gmail.com>
2024-03-19 16:23:18 +0200

    restore multithreading in client test code

--- client.c
+++ client.c
@@ -9,9 +9,9 @@
 #include <netinet/udp.h>
 #include <pthread.h>
 
-#define PORT    8888
-#define MAXLINE 1024
-#define NUM_THREADS 4
+#define PORT          8888
+#define MAX_BUFF_SIZE 1024
+#define NUM_THREADS   4
 
 struct DNSHeader {
     uint16_t id;
@@ -23,75 +23,49 @@ struct DNSHeader {
 };
 
 void* send_thread(void* arg) {
+
+
     int sockfd;
-    char buffer[MAXLINE];
+    char buffer[MAX_BUFF_SIZE];
     struct sockaddr_in servaddr;
 
     if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        perror("Socket creation failed\n");
+        perror("Socket creation failed");
         exit(1);
     }
 
     memset(&servaddr, 0, sizeof(servaddr));
 
     servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = INADDR_ANY;
+    // servaddr.sin_addr.s_addr = INADDR_ANY;
     servaddr.sin_port = htons(PORT);
-
-    int n;
-    socklen_t len = sizeof(servaddr);
+    inet_pton(AF_INET, "127.0.0.1", &servaddr.sin_addr);
 
     while (1) {
-        char query[] = {
+            unsigned char query[] = {
             0x26, 0xa9, 0x01, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00,
             0x00, 0x03, 0x77, 0x77, 0x77, 0x06, 0x67, 0x6f, 0x6f, 0x67, 0x6c,
             0x65, 0x03, 0x63, 0x6f, 0x6d, 0x00, 0x00, 0x01, 0x00, 0x01
         };
 
-        // struct iphdr ipHeader;
-        // ipHeader.ihl = 5;
-        // ipHeader.version = 4; // IPv4
-        // ipHeader.tos = 0; // Type of Service
-        // ipHeader.tot_len = sizeof(struct iphdr) + sizeof(struct udphdr) + sizeof(struct DNSHeader) + sizeof(query);
-        // ipHeader.id = htons(1234); // Random ID
-        // ipHeader.frag_off = 0; // Fragment Offset
-        // ipHeader.ttl = 64; // Time-to-Live
-        // ipHeader.protocol = IPPROTO_UDP; // UDP protocol
-        // ipHeader.check = 0; // Checksum will be computed automatically
-        // ipHeader.saddr = inet_addr("192.168.1.1"); // Source IP
-        // ipHeader.daddr = inet_addr("8.8.8.8"); // Destination IP
-
-        // struct udphdr udpHeader;
-        // udpHeader.source = htons(12345); // Source Port
-        // udpHeader.dest = htons(53); // Destination Port
-        // udpHeader.len = htons(sizeof(struct udphdr) + sizeof(struct DNSHeader) + sizeof(query)); // Length of UDP header + data
-        // udpHeader.check = 0; // Checksum will be computed automatically
-
-        // char packet[sizeof(struct iphdr) + sizeof(struct udphdr) + sizeof(struct DNSHeader) + sizeof(query)];
-
-        // // Copy headers and query into the packet buffer
-        // memcpy(packet, &ipHeader, sizeof(struct iphdr));
-        // memcpy(packet + sizeof(struct iphdr), &udpHeader, sizeof(struct udphdr));
-        // memcpy(packet + sizeof(struct iphdr) + sizeof(struct udphdr), query, sizeof(query));
-
         if (sendto(sockfd, query, sizeof(query), 0,
-               (const struct sockaddr*)&servaddr, sizeof(servaddr)) < 0) {
+                   (const struct sockaddr*)&servaddr, sizeof(servaddr)) < 0) {
             perror("Sendto failed");
             exit(1);
         }
 
         printf("DNS query sent to proxy.\n");
 
-        n = recvfrom(sockfd, buffer, MAXLINE, 0,
-                     (struct sockaddr*)&servaddr, &len);
-        if (n > 0) {
-            buffer[n] = '\0';
+        socklen_t addr_len = sizeof(servaddr);
+        int recv_len = recvfrom(sockfd, buffer, MAX_BUFF_SIZE, 0,
+                                (struct sockaddr*)&servaddr, &addr_len);
+        if (recv_len > 0) {
             printf("Server: %s\n", buffer);
         } else {
             perror("Receive failed");
         }
 
-        usleep(200000);
+        usleep(10000);
     }
 
     pthread_exit(NULL);
@@ -101,15 +75,15 @@ int main(int argc, char** argv) {
     pthread_t threads[NUM_THREADS];
 
     for (int i = 0; i < NUM_THREADS; ++i) {
-        if(pthread_create(&threads[i], NULL, send_thread, NULL) != 0) {
-            perror("Thread creation failed\n");
+        if (pthread_create(&threads[i], NULL, send_thread, NULL) != 0) {
+            perror("Thread creation failed");
             exit(1);
         }
     }
 
     for (int i = 0; i < NUM_THREADS; ++i) {
         if (pthread_join(threads[i], NULL) != 0) {
-            perror("Thread join failed\n");
+            perror("Thread join failed");
             exit(1);
         }
     }
